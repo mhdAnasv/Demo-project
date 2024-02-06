4e9fbd62a49182f36a027a7ae34d0dffc298c735
mhdAnasv <muhammadanas7693@gmail.com>
2024-02-06 21:48:45 +0530

    added1

--- ecommerce/cart/admin.py
+++ ecommerce/cart/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from cart.models import Cart,placeorder,account
-
+from django.contrib import admin
 admin.site.register(Cart)
 admin.site.register(placeorder)
 admin.site.register(account)
